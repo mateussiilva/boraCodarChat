5f2d0ac78ba1b496eff0c51ab772682ebe5ac51d
mateussiilva <mateuspython3010@gmail.com>
2023-02-05 23:35:59 -0200

    Funcionalidade de envio de mensagem

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 
+
+
+function getDataHoraAtual(){
+    const elemento = document.querySelector("#last-seen")
+    const data = new Date()
+    const diaDaSemana = data.toLocaleDateString()
+    const hora = data.toLocaleTimeString('pt-br', {
+        hour12:false,
+    }).split('').slice(0,5).join('')
+    
+    return elemento.innerHTML = `${diaDaSemana} - ${hora}`
+}
+
 const messages = document.querySelector(".messages")
 
-// messages.style.backgroundColor = "red"
-messages.lastElementChild.scrollIntoView()
+try{
+    messages.lastElementChild.scrollIntoView()
+}catch(e){}
+
+
+getDataHoraAtual()
+
+
+const arrayMessages = document.querySelectorAll(".messages")
+
+
+
+document.addEventListener('submit', e => {
+    e.preventDefault() // Bloqueia o envio do envento
+
+    const message = document.querySelector("#input-message").value
+    
+    if (message.length !== 0){
+        console.log(message)
+        message.value = ' '
+        
+    }
+
+})
+
